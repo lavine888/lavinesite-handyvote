5dd2a2bb01d623efad5d0740a374f11ef88b89c4
LavineX <dbest72500@gmail.com>
2026-09-11 17:26:43 +0800

    feat: layer Lavine signature over 3D workbench

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import DigitalWorkbench from "@/components/DigitalWorkbench";
+import LavineSignature from "@/components/LavineSignature";
 
 export default function Home() {
-  return <DigitalWorkbench />;
+  return (
+    <>
+      <DigitalWorkbench />
+      <LavineSignature />
+    </>
+  );
 }
